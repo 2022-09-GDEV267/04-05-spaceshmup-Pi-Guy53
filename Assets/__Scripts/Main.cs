e772e55e63e8b60b4d18784cb03bf916adaf6667
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-10-03 16:01:35 -0400

    enemy spawning and initial layer application

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -3,25 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Main : MonoBehaviour {
-
+public class Main : MonoBehaviour
+{
+    public static Main S;
 
 
     [Header("Set in Inspector")]
-    bool placeholder2; // here to keep VS from freaking out - DELETE IT
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = .5f;
+    public float enemyDefaultPadding = 1.5f;
 
+    private BoundsCheck bndCheck;
 
-
-    
+    private int ndx;
+    private float currentPadding;
 
     private void Awake()
     {
-        
+        S = this;
+    }
+
+    private void Start()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 
     public void SpawnEnemy()
     {
-        
+        ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        currentPadding = enemyDefaultPadding;
+        if (go.GetComponent<BoundsCheck>())
+        {
+            currentPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        go.transform.position = new Vector3(Random.Range(-bndCheck.camWidth + currentPadding, bndCheck.camWidth - currentPadding), bndCheck.camHeight + currentPadding, 0);
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
     }
 
     public void DelayedRestart(float delay)
