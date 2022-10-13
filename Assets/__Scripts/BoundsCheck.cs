63ab8c500bdb57bf20c34df3741b8cf47f459934
Pi-Guy53 <71029215+Pi-Guy53@users.noreply.github.com>
2022-10-13 11:33:20 -0400

    fixed a minor bug with some enemies not despawning

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -35,34 +35,31 @@ public class BoundsCheck : MonoBehaviour
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
-            isOnScreen = false;
             offRight = true;
         }
 
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
-            isOnScreen = false;
             offLeft = true;
         }
 
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
-            isOnScreen = false;
             offUp = true;
         }
 
         if (pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
-            isOnScreen = false;
             offDown = true;
         }
 
         transform.position = pos;
 
         isOnScreen = !(offRight || offLeft || offUp || offDown);
+
         if (keepOnScreen && !isOnScreen)
         {
             transform.position = pos;
