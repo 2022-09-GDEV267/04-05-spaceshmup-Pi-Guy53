dacfde918d09bb2563fc11767328a85ca78d9ed9
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-10-17 19:29:48 -0400

    started on adding Enemy_5 a boss enemy, going to be some sort of Dreadnought class, high health, multiple pieces to destroy

--- Assets/__Scripts/Enemy_5.cs
+++ Assets/__Scripts/Enemy_5.cs
@@ -2,17 +2,108 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_5 : MonoBehaviour
+public class Enemy_5 : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<WingArmor> pieces;
+    public GameObject wingParent;
+
+    private Vector3 pos1, pos2;
+    private float timeStart, duration = 4;
+
+    private void Start()
     {
-        
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            pieces[i].setUp(this);
+        }
+
+        pos2 = transform.position;
+        setMovePoints();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Move()
     {
-        
+        float u = (Time.time - timeStart) / duration;
+
+        if (u >= 1)
+        {
+            setMovePoints();
+            u = 0;
+        }
+
+        u = 1 - Mathf.Pow(1 - u, 2);
+        pos = ((1 - u) * pos1) + (u * pos2);
+
+        wingParent.transform.position = transform.position;
+    }
+
+    void setMovePoints()
+    {
+        pos1 = pos2;
+
+        pos2.x = Random.Range(-bndCheck.camWidth / 2, bndCheck.camWidth / 2);
+        pos2.y = Random.Range(bndCheck.camHeight / 2, bndCheck.camHeight);
+
+        timeStart = Time.time;
+    }
+
+    public void wingDestroyed(WingArmor lostWing)
+    {
+        pieces.Remove(lostWing);
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+
+        if (otherGO.tag == "ProjectileHero")
+        {
+            if (pieces.Count <= 0)
+            {
+                Projectile p = otherGO.GetComponent<Projectile>();
+                if (!bndCheck.isOnScreen)
+                {
+                    Destroy(otherGO);
+                    return;
+                }
+
+                ShowDamage();
+
+                health -= Main.GetWeaponDefinition(p.type).damage;
+
+                if (health <= 0)
+                {
+                    if (!notifiedOfDestruction)
+                    {
+                        Main.S.ShipDestroyed(this);
+                        notifiedOfDestruction = true;
+                    }
+
+                    Destroy(gameObject);
+                }
+            }
+
+            Destroy(otherGO);
+            return;
+        }
+    }
+
+    void ShowDamage()
+    {
+        foreach (Material m in materials)
+        {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage()
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
     }
 }
--- Assets/__Scripts/WingArmor.cs
+++ Assets/__Scripts/WingArmor.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class WingArmor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health;
+
+    public float showDamageDuration = .1f;
+    private Color[] originalColors;
+    private Material[] materials;
+
+    private bool showingDamage;
+    public float damageDoneTime;
+
+    private Enemy_5 parentShip;
+
+    private void Awake()
     {
-        
+        materials = Utils.GetAllMaterials(gameObject);
+        originalColors = new Color[materials.Length];
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            originalColors[i] = materials[i].color;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setUp(Enemy_5 _parentShip)
     {
-        
+        parentShip = _parentShip;
+    }
+
+    private void Update()
+    {
+        if (showingDamage && Time.time > damageDoneTime)
+        {
+            UnShowDamage();
+        }
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+
+        if (otherGO.tag == "ProjectileHero")
+        {
+            Projectile p = otherGO.GetComponent<Projectile>();
+
+            ShowDamage();
+
+            health -= Main.GetWeaponDefinition(p.type).damage;
+
+            if (health <= 0)
+            {
+                parentShip.wingDestroyed(this);
+                Destroy(gameObject);
+            }
+
+            Destroy(otherGO);
+            return;
+        }
+    }
+
+    void ShowDamage()
+    {
+        foreach (Material m in materials)
+        {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage()
+    {
+        for (int i = 0; i < materials.Length; i++)
+        {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
     }
 }
